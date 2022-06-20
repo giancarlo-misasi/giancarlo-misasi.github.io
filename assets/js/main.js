7ac12081fb64f6baa29e89e9d17f912ba5ab286a
Giancarlo Misasi <gmisasi.cloud>
2022-06-20 22:39:20 +0000

    Changed floating sentences to CS terminology.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Load senteces and start the animation
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/assets/json/pastas.json");
+    xhr.open('GET', "/assets/json/sentences.json");
     xhr.onreadystatechange = function (data) {
       if(xhr.readyState == 4 && xhr.status == 200) {
-        params.sentences = JSON.parse(data.currentTarget.response).pastas;
+        params.sentences = JSON.parse(data.currentTarget.response).sentences;
         sentenceScroller(params).draw();
       }
     };
