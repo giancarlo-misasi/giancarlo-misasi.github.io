e913e2c8d63de7718d1d18f50c5c13409a7c288d
Giancarlo Misasi <gmisasi.cloud>
2022-06-22 07:28:33 +0000

    Refactored includes, updated css.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var params = {
-        canvasId: "splash",
-        backgroundColor: "#000000",
-        fontFamily: 'arial',
-        fontSizeVmin: 2,
-        color: "#FFFFFF"
-    };
+document.addEventListener("DOMContentLoaded", function () {
+  var sentence_params = {
+    json_file: "/assets/json/sentences.json",
+    canvasId: "splash",
+    backgroundColor: "#000000",
+    fontFamily: 'arial',
+    fontSizeVmin: 2,
+    color: "#FFFFFF"
+  };
 
-    // Load senteces and start the animation
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/assets/json/sentences.json");
-    xhr.onreadystatechange = function (data) {
-      if(xhr.readyState == 4 && xhr.status == 200) {
-        params.sentences = JSON.parse(data.currentTarget.response).sentences;
-        sentenceScroller(params).draw();
-      }
-    };
-    xhr.send();
+  var carousel_params = {
+    type: 'loop',
+    perPage: 3,
+    autoplay: true,
+    interval: 3000
+    // arrows: false
+  };
 
-    // Setup smooth scrolling
-    new SweetScroll({ });
+  // Load senteces and start the animation
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', sentence_params.json_file);
+  xhr.onreadystatechange = function (data) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      sentence_params.sentences = JSON.parse(data.currentTarget.response).sentences;
+      sentenceScroller(sentence_params).draw();
+    }
+  };
+  xhr.send();
+
+  // Setup carousel
+  var splides = document.querySelectorAll('.splide');
+  if (splides.length) {
+    for (var i = 0; i < splides.length; ++i) {
+      var splideElement = splides[i];
+      var splide = new Splide(splideElement, carousel_params);
+      splide.mount();
+    }
+  }
 });
